b08ff27d8dd2130c6da81d4eb3d85fdd2085221c
ananya-agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-04-19 23:25:58 +0530

    Create structurally_identical.cpp

---
+++ Data Structure in C++/Lecture 11 : Trees/Code/structurally_identical.cpp
@@ -0,0 +1,40 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+
+bool areIdentical(TreeNode<int> *root1, TreeNode<int> * root2) 
+{
+    if(root1->data != root2->data)
+        return false;
+    
+    if(root1->children.size() != root2->children.size())
+        return false;
+    
+    bool smallans = true;
+    for(int i=0;i<root1->children.size();i++)
+    {
+        smallans = areIdentical(root1->children[i],root2->children[i]);
+        if(smallans == false)
+ 			return smallans;
+    }
+    return smallans;
+}
