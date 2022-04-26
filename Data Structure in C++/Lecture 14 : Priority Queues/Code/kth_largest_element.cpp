32335732b8dd032459b3605902d9cb3e9a690544
ananya-agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-04-26 22:02:52 +0530

    Create kth_largest_element.cpp

---
+++ Data Structure in C++/Lecture 14 : Priority Queues/Code/kth_largest_element.cpp
@@ -0,0 +1,12 @@
+#include<queue>
+int kthLargest(int* arr, int n, int k) 
+{
+ 	priority_queue<int> pq;
+    for(int i=0;i<n;i++)
+        pq.push(arr[i]);
+    
+    for(int i=0;i<k-1;i++)
+        pq.pop();
+    
+    return pq.top();
+}
