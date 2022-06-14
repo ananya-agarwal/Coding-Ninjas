b600049286809f8c8ca47689d9c3473157939703
ananya-agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-06-14 22:25:07 +0530

    Create num_pattern_2.cpp

---
+++ Introduction to C++/Lecture 3 : Conditionals and Loops/Code/num_pattern_2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n,i=1,u=1,j,k;
+cin>>n;
+while(i<=n)
+{
+	k=1;
+	while(k <= n-i)
+	{
+		cout<<" ";
+		k++;
+	}
+	j=1;
+	u=i;
+    while(j<=i)
+    {
+        cout<<u;
+        u++;
+        j++;
+    }
+cout<<endl;
+i++;
+}
+}
+
