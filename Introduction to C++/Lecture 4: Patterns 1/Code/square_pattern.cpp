8ee8069c0b6b4ec095ef431988c581087f9b3214
ananya-agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-06-19 22:30:18 +0530

    Create square_pattern.cpp

---
+++ Introduction to C++/Lecture 4: Patterns 1/Code/square_pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i=1,j,n;
+	cin>>n;
+	while(i<=n)
+	{
+		j=1;
+		while(j<=n)
+		{
+			cout<<n;
+			j++;
+		}
+		cout<<endl;
+    	i++;
+	}
+
+	
+}
