1159a922fa693dd3a1a2d393cb1b648eea9499e5
ananya-agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-06-19 22:58:01 +0530

    Create find_unique.cpp

---
+++ Introduction to C++/Lecture 8: Arrays/Code/find_unique.cpp
@@ -0,0 +1,15 @@
+int findUnique(int *a, int n)
+{
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        int flag = 0;
+        for(j=0;j<n;j++)
+        {
+            if((a[i] == a[j]) && (j != i))
+                flag=1; 
+        }
+             if(flag == 0)
+             return a[i];  
+    }
+}
